aebe0b1adaa1c3f45299a0a4b2633e31a2ef65ed
Mendelt Siebenga <msiebenga@gmail.com>
2020-12-01 23:27:48 +0100

    parse url parameters example

--- examples/ch03-request-response/src/main.rs
+++ examples/ch03-request-response/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+// ANCHOR: example
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/:some/:parameters").get(url_params);
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
+
+async fn url_params(request: Request<()>) -> tide::Result {
+    Ok(format!(
+        "Hello, the url contained {} and {}",
+        request.param("some").unwrap(),
+        request.param("parameters").unwrap()
+    )
+    .into())
+}
+
+// ANCHOR END: example

079bda2a568e64e587bceeb49bb70def25b282a5
Mendelt Siebenga <msiebenga@gmail.com>
2020-12-02 22:12:29 +0100

    request response chapter, add query string examples

--- examples/ch03-request-response/src/main.rs
+++ examples/ch03-request-response/src/main.rs
@@ -1,4 +1,6 @@
 // ANCHOR: example
+use serde::Deserialize;
+use std::collections::HashMap;
 use tide::Request;
 
 #[async_std::main]
@@ -6,6 +8,8 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     app.at("/:some/:parameters").get(url_params);
+    app.at("/query_params").get(query_params);
+    app.at("/simple_query").get(simple_query);
 
     app.listen("127.0.0.1:8080").await?;
     Ok(())
@@ -16,6 +20,32 @@ async fn url_params(request: Request<()>) -> tide::Result {
         "Hello, the url contained {} and {}",
         request.param("some").unwrap(),
         request.param("parameters").unwrap()
+    )
+    .into())
+}
+
+#[derive(Deserialize)]
+struct Query {
+    pub parameter1: String,
+    pub parameter2: i32,
+}
+
+async fn query_params(request: Request<()>) -> tide::Result {
+    let query: Query = request.query()?;
+
+    Ok(format!(
+        "Hello, the query parameters were {} and {}",
+        query.parameter1, query.parameter2,
+    )
+    .into())
+}
+
+async fn simple_query(request: Request<()>) -> tide::Result {
+    let query: HashMap<String, String> = request.query()?;
+
+    Ok(format!(
+        "Hello, the query parameters were {} and {}",
+        query["parameter1"], query["parameter2"],
     )
     .into())
 }
